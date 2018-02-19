a10767e33218cc88a95e00274d6bf541004dc1f4
Vladimir Tarasenko <vladimirtarasenko33@gmail.com>
2018-02-19 22:22:36 +0300

    3d commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 	public static void main (String[] Args) {
 	
 	
-	System.out.println("Hello World from Dev branch");
+	System.out.println("Hello World from master branch");
 		
 		}
 
