49760674bc38549a686b6a52a36b602e10256fc4
Vladimir Tarasenko <vladimirtarasenko33@gmail.com>
2018-02-22 20:24:56 +0300

    Task2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,22 +4,27 @@ public class HelloWorld {
 int arr[]= {12, 55, 7, 11, 9, 43, 14, 8, 32, 15};
 int i;
 int minvalue=arr [0];
-int maxvalue=arr[0];;
+int maxvalue=arr[0];
+int imin = 0;
+int imax=0;
 for(i=0;i<10;i++) {
 	if(arr[i]<minvalue) {
 		minvalue=arr[i];
+		imin=i;
 			}
 	if(arr[i]>maxvalue){
 		maxvalue=arr[i];
+		imax=i;
 	}
 }
-	System.out.println("Мин. значение -"+minvalue);
-    System.out.print("Макс. значение -"+maxvalue);
-	
-		
-}
+	arr[imin]=0;
+	arr[imax]=99;
 
-	
+for(i=0;i<10;i++) {
+	System.out.print(arr[i]+" ");
+    	
+}
+}
 }
 
 
