cb9f86a782f2665d6907e13ca57d63a8d2850361
Vladimir Tarasenko <vladimirtarasenko33@gmail.com>
2018-02-22 21:53:22 +0300

    Task4

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,26 +1,25 @@
 public class HelloWorld {
 	public static void main (String[] Args) {
 	
-int arr[]= {12, 55, 7, 12, 9, 8, 14, 8, 32, 12};
+int arr[]= {14, 55, 7, 12, 9, 8, 14, 10, 32, 12};
 int i;
-int counts=1;
-int digit=0;
-int j;
-for(i=0;i<9;i++) {
-	if(arr[i]!=digit) {
-	for(j=i+1;j<10;j++) {
-		if(arr[i]==arr[j]) {
-		counts++;
-		digit=arr[i];	
-				}		
-			}
-	if(counts!=1) {
-		System.out.println("["+digit+"]"+"встречается" +counts+ " раз");
-	}
-	}	
-	counts=1;	  	
+int temp;
+int arrmiddle=arr.length/2;
+int arrlength=arr.length-1;
+for(i=0;i<10;i++) {
+	System.out.print(arr[i]+" ");
+}
+for(i=0;i<arrmiddle;i++) {
+	temp=arr[i];
+	arr[i]=arr[arrlength-i];
+	arr[arrlength-i]=temp;
+}
+System.out.println();
+for(i=0;i<10;i++) {
+	System.out.print(arr[i]+" ");
+}		
 }
 }
-}
+
 
 
